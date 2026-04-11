4b30765af12cef9e5f626879706b25eaa7da79f5
Razeem <rrazeem07@gmail.com>
2026-04-11 13:13:25 +0530

    settings updated

--- elister/settings.py
+++ elister/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d8ow917r!o+0ysib5nwrszkn7c+y9nlwn4)hu%#+#gf-kp-^^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
